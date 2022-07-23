0497e04c747ad4d72b9f738be1287659b1a76e44
KiranGopalakrishnan <kirangplkrishnan@gmail.com>
2022-07-23 15:08:33 -0400

    fix linting issues

--- apps/backend/prisma/migrations/20210511025552_unique_token/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[token]` on the table `AccessTokens` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "AccessTokens.token_unique" ON "AccessTokens"("token");
--- apps/backend/prisma/migrations/20210517012306_remove_unique_constraint_on_access_tokens/migration.sql
+++
@@ -1,2 +0,0 @@
--- DropIndex
-DROP INDEX "userId_token_status_unique_key";
